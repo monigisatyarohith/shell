768abcb913f50e32586250cddde3d778de71b8b4
rohith <msatyarohith@gmail.com>
2026-01-01 06:43:06 +0000

    added all files

--- armstrong.sh
+++ armstrong.sh
@@ -5,14 +5,12 @@ read n
 rem=0
 rev=0
 temp=$n
-while [ $n -ne 0 ]
-do
+while [ $n -ne 0 ];do
 rem=$((n%10))
 rev=$((rev+rem*rem*rem))
 n=$((n/10))
 done
-if [ $temp -eq $rev ]
-then
+if [ $temp -eq $rev ];then
 echo "$temp is a armstrong number"
 else
 echo "$temp is not a armstrong number"
